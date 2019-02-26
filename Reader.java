aefd237e4e303d4f8ea82912c9b97e850ebab26c
Qiong Huang <qhuang00@stuy.edu>
2019-02-26 10:17:13 -0500

    need to fix array out of bounds error

--- Reader.java
+++ Reader.java
@@ -3,11 +3,32 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Reader{
   public static void main(String[] args) throws FileNotFoundException{
+    char[][] maze;
     File text = new File("Mazel.txt");
     Scanner s = new Scanner(text);
+    int rows = 0;
+    int cols = 0;
     while(s.hasNextLine()){
           String line = s.nextLine();
-          System.out.println(line);//hopefully you can do other things with the line
+          cols = line.length();
+          rows++;
       }
+    maze = new char[rows][cols];
+    s = new Scanner(text);
+    int line = 0;
+    while(s.hasNextLine()){
+      String t = s.nextLine();
+      for (int i = 0; i < cols ;i++ ) {
+        maze[line][i] = t.charAt(i);
+        line++;
+      }
+    }
+    for (char[] row: maze) {
+      for (char c: row) {
+        System.out.print(c);
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
   }
 }
